3fb8332c23ec47f51006bbd4c6a08b14e7453d55
Jean Uchoa <jean.uchoa@gmail.com>
2023-02-03 12:18:14 -0300

    refctoring

--- zsh/lang/general.sh
+++ zsh/lang/general.sh
@@ -1,3 +1,4 @@
+alias cat="bat"
 alias t="tmux"
 alias ls="ls -G --color"
 alias g="git"
@@ -8,18 +9,12 @@ alias externalkbd='setxkbmap -layout us -variant intl'
 alias notekbd='setxkbmap -model abnt2 -layout br'
 tnewsession() { tmux new-session -s $1 }
 download() { http get $1 --download }
-xmldownload() { http get $1 --follow -b | xmlstarlet fo >> $2 }
+xmldownload() { http get $1 --follow | xmlstarlet fo >> $2 }
+ta() { [ -z "$1"] && tmux attach || tmux attach -t $1 }
 notify() {
   local url="https://api.pushover.net/1/messages"
   local user="u9sh4ah5kdmwd5utk5vsszqgwpth1p"
   local token="ajhkz4dqk5a3gnkckqi33cjmo7pwu6"
 
   http post $url user=$user token=$token message=$*
-}
-ta() {
-  if [ -z "$1"]; then
-    tmux attach
-  else
-    tmux attach -t $1
-  fi
 }

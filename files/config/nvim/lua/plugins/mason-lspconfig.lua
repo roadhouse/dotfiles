58e1f648921709c8efaf9ff9f1ec998f62ef9634
Jean Uchoa <jean.uchoa@gmail.com>
2023-02-28 19:08:06 -0300

    add 'vim' as global on lua lsp

--- files/config/nvim/lua/plugins/mason-lspconfig.lua
+++ files/config/nvim/lua/plugins/mason-lspconfig.lua
@@ -1,10 +1,23 @@
 local mason = require('mason-lspconfig')
 local lspconfig = require('lspconfig')
+
+local langconf = {
+  lua_ls = {
+    Lua = {
+      diagnostics = {
+        globals = { "vim" },
+      },
+    },
+  },
+}
+
 mason.setup({
   automatic_installation = true,
 })
 mason.setup_handlers({
   function(server)
-    lspconfig[server].setup({})
+    lspconfig[server].setup({
+      settings = langconf[server]
+    })
   end,
 })

9454023895ed63a247767fc58e4049a305d2cb79
Jean Uchoa <jean.uchoa@gmail.com>
2023-03-14 21:33:58 -0300

    update colorscheme setup

--- files/config/nvim/lua/colorscheme.lua
+++ files/config/nvim/lua/colorscheme.lua
@@ -1,8 +1,18 @@
----
--- Colorscheme
----
-vim.cmd('colorscheme tokyonight-night')
-vim.cmd('highlight Normal ctermbg=NONE guibg=NONE')
-vim.cmd('highlight NonText ctermbg=NONE guibg=NONE')
+require("tokyonight").setup({
+  style = "storm",
+  transparent = true,
+  terminal_colors = true,
+  styles = {
+    comments = { italic = true },
+    keywords = { italic = true },
+    functions = { italic = true },
+    sidebars = "transparent",
+    floats = "transparent",
+  },
+  hide_inactive_statusline = true,
+  lualine_bold = true,
+})
 
---vim.g.tokyonight_dark_float = false
+vim.cmd('colorscheme tokyonight')
+--vim.cmd('highlight Normal ctermbg=NONE guibg=NONE')
+--vim.cmd('highlight NonText ctermbg=NONE guibg=NONE')

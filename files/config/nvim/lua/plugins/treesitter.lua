88503eb20aa2062a56a21d2d4283085ac8f4f312
Jean Uchoa <jean.uchoa@gmail.com>
2024-03-22 09:56:49 -0300

    change treessiter mapping

--- files/config/nvim/lua/plugins/treesitter.lua
+++ files/config/nvim/lua/plugins/treesitter.lua
@@ -8,8 +8,8 @@ require'nvim-treesitter.configs'.setup {
   incremental_selection = {
     enable = true,
     keymaps = {
-      init_selection = '<C-space>',
-      node_incremental = '<C-space>',
+      init_selection = '<C-y>',
+      node_incremental = '<C-y>',
       scope_incremental = '<Tab>',
       node_decremental = '<S-Tab>',
     },

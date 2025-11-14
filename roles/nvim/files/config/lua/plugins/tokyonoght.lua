286b768f6370186a1f0cde1da1f6ebba1757c2b6
Jean Uchoa <jean.uchoa@gmail.com>
2025-11-13 23:17:17 -0300

    fix transparency

--- roles/nvim/files/config/lua/plugins/tokyonoght.lua
+++ roles/nvim/files/config/lua/plugins/tokyonoght.lua
@@ -2,15 +2,25 @@ return {
   "folke/tokyonight.nvim",
   lazy = false,
   priority = 1000,
-  opts = {
-    style = "storm",
-    transparent = true,
-    styles = {
-      functions = { italic = true },
-      sidebars = "transparent",
-      floats = "transparent",
-    },
-    hide_inactive_statusline = true,
-    lualine_bold = true,
-  },
+  config = function()
+    require("tokyonight").setup({
+      style = "storm",
+      transparent = true,
+      styles = {
+        functions = { italic = true },
+        sidebars = "transparent",
+        floats = "transparent",
+      },
+      hide_inactive_statusline = true,
+      lualine_bold = true,
+      on_highlights = function(hl, c)
+        hl.CmpPmenu = { fg = c.fg_dark, bg = "NONE" }
+        hl.CmpPmenuBorder = { fg = c.border_highlight, bg = "NONE" }
+        hl.CmpSel = { fg = c.bg_dark, bg = c.blue, bold = true }
+        hl.CmpDocSel = { fg = c.bg_dark, bg = c.blue, bold = true }
+        hl.CmpDoc = { fg = c.fg_dark, bg = "NONE" }
+        hl.CmpDocBorder = { fg = c.border_highlight, bg = "NONE" }
+      end,
+    })
+  end,
 }
